caef677c7fd8f61b8c71487858c5420a5087272d
Basil, Ugo <115357283+thebasilugo@users.noreply.github.com>
2022-12-11 18:57:07 +0000

    java intro

---
+++ NewMain.java
@@ -0,0 +1,16 @@
+
+
+/**
+ *
+ * @author thebasilugo
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
